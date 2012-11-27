62512ee1930ad50e2ae8d97497106b34ed9fea1e
pascalc <pascal.chevrel@free.fr>
2012-11-27 17:19:28 +0100

    update script

--- installBedrock.sh
+++ installBedrock.sh
@@ -9,33 +9,59 @@ echo "We assume that you have recently forked bedrock on github and will use tha
 echo "If you are in a Virtual Machine, don't forget to add your ssh key on it"
 echo "Please provide your github user name below: "
 read repo
-echo "Sudo mode, install Node.js, Subversion, Git, npm, virtualenv. (if they were not already installed)"
-#sudo apt-get install subversion git nodejs npm python-virtualenv python-jinja2 python-bcrypt
-sudo apt-get install subversion git nodejs npm python-virtualenv python-dev
+
+echo "Do you need to install subversion git nodejs npm python-virtualenv python-dev (sudo password needed)? (y/n)"
+read -n 1 globaldependencies
+echo ""
+if [ $globaldependencies == 'y' ]
+then
+    echo "Sudo mode, install Node.js, Subversion, Git, npm, virtualenv. (if they were not already installed)"
+    sudo apt-get install subversion git nodejs npm python-virtualenv python-dev
+    ./venv/bin/pip install -r requirements/dev.txt   # installs dev dependencies
+fi
+
 echo "git@github.com:${repo}/bedrock.git"
 git clone --recursive git@github.com:${repo}/bedrock.git
 #git clone --recursive https://${repo}@github.com/${repo}/bedrock.git
+
 cd ./bedrock
+
+echo "git://github.com/mozilla/bedrock.git added as upstream remote "
 git remote add upstream git://github.com/mozilla/bedrock.git
-echo "Sudo mode, install Bedrock dependencies"
+
 echo "Create a virtual environement in the folder venv"
 virtualenv venv                            # create a virtual env in the folder `venv`
-echo "activate the virtual env"
+echo "Activate the virtual env"
 source venv/bin/activate                   # activate the virtual env
-echo "Install Bedrock dependencies"
+echo "Install Bedrock local dependencies in venv"
 ./venv/bin/pip install -r requirements/compiled.txt   # installs compiled dependencies
-#./venv/bin/pip install -r requirements/dev.txt   # installs dev dependencies
-echo "Install Bedrock dependencies"
+
+echo "Do you want to install developper dependencies to be able to run tests locally and participate to documentation? (y/n)"
+read -n 1 devdependencies
+echo ""
+if [ $devdependencies == 'y' ]
+then
+
+    echo "Installing developer dependencies..."
+    ./venv/bin/pip install -r requirements/dev.txt   # installs dev dependencies
+fi
+
+echo "Install more Bedrock dependencies"
 ./venv/bin/pip install jinja2 django-bcrypt
+
 echo "Copy settings/local.py-dist into settings/local.py"
 cp settings/local.py-dist settings/local.py
+
 echo "Check out the latest product-details"
 ./manage.py update_product_details
+
 echo "Install the less compiler"
 sudo npm install -g less
+
 echo -e "\nLESS_BIN = '/usr/local/bin/lessc'" >> settings/local.py
-#echo 'ROOT_URLCONF = "urls"' >> settings/local.py
-echo "Check out all the translation which live on svn in the localizers repository"
+
+echo "Check out all the translations which live on svn in the localizers repositories"
+
 mkdir locale
 cd locale
 svn co https://svn.mozilla.org/projects/mozilla.com/trunk/locales/ .
